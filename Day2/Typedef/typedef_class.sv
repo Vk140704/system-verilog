1385714f79cfa3afc158633cfea2a94606e2cc38
Vishalpalanisamy <vishal.nkl04@gmail.com>
2026-01-06 13:02:33 +0530

    Create typedef_class.sv

---
+++ Day2/Typedef/typedef_class.sv
@@ -0,0 +1,23 @@
+// Code your testbench here
+// or browse Examples
+typedef class fruit2;
+  class fruit1;
+    fruit2 f ;
+  endclass
+  
+  class  fruit2;
+    fruit1 f;
+  endclass
+  
+  module typedefclass;
+    
+    initial begin
+      
+      fruit1 class1;
+      fruit2 class2;
+      
+      $display("\n typedef class example ");
+      $display("\n fruit2 class usage  with typedef to avoid compile error");
+      $display("");
+    end
+  endmodule: typedefclass
