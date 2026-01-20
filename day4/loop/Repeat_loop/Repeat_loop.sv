4aaafddb02f0955e01bb387b8d399d6e72100ce1
Vishalpalanisamy <vishal.nkl04@gmail.com>
2026-01-20 11:15:24 +0530

    Create Repeat_loop.sv

---
+++ day4/loop/Repeat_loop/Repeat_loop.sv
@@ -0,0 +1,12 @@
+// Code your testbench here
+// or browse Examples
+module repeat_code;
+  initial begin
+    
+    repeat(5)begin
+      $display("this is repeat output ");
+      $display("ur offerletter is coming soon");
+      $display("------------------------------");
+    end
+  end
+    endmodule
